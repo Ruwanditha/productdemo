e1311db8723e59ec97d43916e1c583447ee984a8
ruwanditha <piumal512@gmail.com>
2019-08-18 21:27:15 +0530

    TestC1

---
+++ productdemo/src/test/java/lk/productd/productdemo/ProductdemoApplicationTests.java
@@ -0,0 +1,16 @@
+package lk.productd.productdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProductdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
