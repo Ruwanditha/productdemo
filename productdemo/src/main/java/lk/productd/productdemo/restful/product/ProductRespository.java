3c740fd78efbb44479abf9e04152b61ba87ccfa7
ruwanditha <piumal512@gmail.com>
2019-08-25 11:38:36 +0530

    ruwanditha_remove_compo_ano_from_repo

--- productdemo/src/main/java/lk/productd/productdemo/restful/product/ProductRespository.java
+++ productdemo/src/main/java/lk/productd/productdemo/restful/product/ProductRespository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-@Component
 @Repository
 public interface ProductRespository extends JpaRepository<Product, Long> {
 	
